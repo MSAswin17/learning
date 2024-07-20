f426705ac0f34fa93e8fd7e505b930bdd4793528
MSAswin17 <aswinms1706@gmail.com>
2024-07-20 18:48:12 +0530

    This is my First version

---
+++ src/main/java/com/java/firstfile/FirstfileApplication.java
@@ -0,0 +1,13 @@
+package com.java.firstfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstfileApplication.class, args);
+	}
+
+}
